dba7382677f62f0ee2e2d2269964d7c9ac9c5c36
BPplays <andymalbp@gmail.com>
2024-08-27 22:03:09 -0700

    change ini

--- install-arch.sh
+++ install-arch.sh
@@ -138,10 +138,10 @@ mount "${EFI_PARTITION}" /mnt/boot/efi
 # show the mounted partitions
 lsblk
 
-yes | pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-yes | pacman-key --lsign-key 3056513887B78AEB
-yes | pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-yes | pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | tee -a /etc/pacman.conf
 
