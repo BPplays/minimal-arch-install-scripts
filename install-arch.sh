efe35240dcd7813d28fdf7d2d83b64dd0c4ae1da
BPplays <andymalbp@gmail.com>
2024-08-28 15:26:49 -0700

    change lts

--- install-arch.sh
+++ install-arch.sh
@@ -162,7 +162,8 @@ pacman -Syy --noconfirm archlinux-keyring
 
 
 # install necessary packages
-pacstrap -K /mnt base base-devel linux linux-headers linux-lts linux-lts-headers linux-firmware lvm2 vim git networkmanager refind os-prober efibootmgr iwd amd-ucode crudini cryptsetup
+# pacstrap -K /mnt base base-devel linux linux-headers linux-lts linux-lts-headers linux-firmware lvm2 vim git networkmanager refind os-prober efibootmgr iwd amd-ucode crudini cryptsetup
+pacstrap -K /mnt base base-devel linux linux-headers linux-firmware lvm2 vim git networkmanager refind os-prober efibootmgr iwd amd-ucode crudini cryptsetup
 
 # refind-install hook
 cat <<EOF >/etc/pacman.d/hooks/refind.hook
