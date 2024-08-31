f6877b81e3f269b7c4751d004d76cae195bf4c2b
BPplays <andymalbp@gmail.com>
2024-08-30 19:49:32 -0700

    yay aur only freeipa

--- chroot-script.sh
+++ chroot-script.sh
@@ -37,7 +37,7 @@ TMPDIR=$(mktemp -d)
 git clone https://aur.archlinux.org/freeipa.git $TMPDIR
 gpg --import $TMPDIR/keys/pgp/*asc
 rm -fr $TMPDIR
-yay -Sua --sudoloop --noconfirm --aur https://aur.archlinux.org/freeipa.git
+yay -Sua --sudoloop --noconfirm --aur freeipa-client
 
 
 
