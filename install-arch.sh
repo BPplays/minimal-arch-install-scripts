5fa648efa6eb62ce4920831196272d1752e1d142
BPplays <suzuko.oss@proton.me>
2026-09-17 19:48:10 -0700

    better partition control

--- install-arch.sh
+++ install-arch.sh
@@ -805,13 +805,16 @@ else
 		-n 3:0:+$(awk "BEGIN {print int(${arch_size_KIB})}")kib -t 3:8309 -c 3:"Arch Linux" \
 		"${BLOCK_DEVICE}"
 
-	# EFI_PARTITION="${BLOCK_DEVICE}p1"
-	# BOOT_PARTITION="${BLOCK_DEVICE}p2"
-	# NEW_PARTITION="${BLOCK_DEVICE}p3"
+	EFI_PARTITION="${BLOCK_DEVICE}p1"
+	BOOT_PARTITION="${BLOCK_DEVICE}p2"
+	NEW_PARTITION="${BLOCK_DEVICE}p3"
 
-	EFI_PARTITION=$(select_partition "Choose the EFI system partition") || exit 1
-	BOOT_PARTITION=$(select_partition "Choose the boot partition" "$EFI_PARTITION") || exit 1
-	NEW_PARTITION=$(select_partition "Choose the LUKS partition" "$EFI_PARTITION" "$BOOT_PARTITION") || exit 1
+
+	echo "partitioning:"
+	echo "EFI  (fat32): $EFI_PARTITION"
+	echo "BOOT (ext4):  $BOOT_PARTITION"
+	echo "main (any):   $NEW_PARTITION"
+	read -r -p "Press Enter to continue..."
 
 	mkfs.fat -F32 "$EFI_PARTITION"
 	mkfs.ext4 -F -m 2 "$BOOT_PARTITION"
